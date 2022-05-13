d49d679ed4749bc35794893c6700066e8782e01e
Juangsalaz <juang.salaz.prabowo@gmail.com>
2022-05-13 10:06:52 +0700

    first commit

---
+++ apps/home/models.py
@@ -0,0 +1,17 @@
+# -*- encoding: utf-8 -*-
+"""
+Copyright (c) 2019 - present AppSeed.us
+"""
+
+from django.db import models
+from django.contrib.auth.models import User
+
+# Create your models here.
+
+class Role(models.Model):
+    name = models.CharField(max_length=255)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.title
